8e6f7a39625adf19d2066a2c5fc3161609e7b05f
mike <mikelewek@gmail.com>
2015-11-08 18:06:46 -0500

    style: change for PEP8

--- tournament.py
+++ tournament.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# 
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -16,16 +15,18 @@ def deleteMatches():
     conn = connect()
     c = conn.cursor()
     c.execute("DELETE FROM matches;")
-    conn.commit() 
+    conn.commit()
     conn.close()
+
 
 def deletePlayers():
     """Remove all the player records from the database."""
     conn = connect()
     c = conn.cursor()
     c.execute("DELETE FROM players;")
-    conn.commit() 
+    conn.commit()
     conn.close()
+
 
 def countPlayers():
     """Returns the number of players currently registered."""
@@ -36,26 +37,28 @@ def countPlayers():
     c.close
     return cnt[0]
 
+
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
     conn = connect()
     c = conn.cursor()
     c.execute("INSERT INTO players (name) VALUES (%s)", (name,))
-    conn.commit() 
+    conn.commit()
     conn.close()
 
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
+    The first entry in the list should be the
+    player in first place, or a player
     tied for first place if there is currently a tie.
 
     Returns:
@@ -67,10 +70,12 @@ def playerStandings():
     """
     conn = connect()
     c = conn.cursor()
-    c.execute("SELECT id, name, cast(wins as integer), cast(total as integer) FROM standings;")
+    c.execute("""SELECT id, name, cast(wins as integer), cast(total as integer)
+              FROM standings""")
     results = c.fetchall()
     c.close()
     return results
+
 
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
@@ -81,18 +86,20 @@ def reportMatch(winner, loser):
     """
     conn = connect()
     c = conn.cursor()
-    c.execute("INSERT INTO matches (winner, loser) VALUES (%s, %s)", (winner, loser,))
-    conn.commit() 
+    c.execute("""INSERT INTO matches(winner, loser)
+              VALUES('%s', '%s')""", (winner, loser,))
+    conn.commit()
     c.close()
+
 
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -102,15 +109,17 @@ def swissPairings():
     """
     conn = connect()
     c = conn.cursor()
-    c.execute("SELECT id, name, cast(wins as integer), cast(total as integer) FROM standings;")
+    c.execute("""SELECT id, name, cast(wins as integer), cast(total as integer)
+              FROM standings""")
     result = c.fetchall()
     c.close()
 
-    #run through every two results and append to list
+# run through every two results and append to list
     pairings = []
     i = 0
     while i < len(result):
-        pairings.append((result[i][0], result[i][1], result[i + 1][0], result[i + 1][1]))
+        pairings.append((result[i][0], result[i][1],
+                        result[i + 1][0], result[i + 1][1]))
         i = i + 2
 
     return pairings
